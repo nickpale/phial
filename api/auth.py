ac80fa0af0ea86ea5c2182b5cd0155617775566f
= <cfcarter55@gmail.com>
2021-03-31 15:45:05 +0800

    added templates and finished register bp

--- api/auth.py
+++ api/auth.py
@@ -29,9 +29,9 @@ def register():
 
         if error is None:
             uid = uuid.uuid4()
-            str(uid)
+            uid = str(uid)
             db.execute(
-                'INSERT INTO user (userID, username, password) VALUES (?, ?)',
+                'INSERT INTO user (userID, username, password) VALUES (?, ?, ?)',
                 (uid, username, generate_password_hash(password))
             )
             db.commit()
@@ -41,4 +41,38 @@ def register():
 
     return render_template('auth/register.html')
 
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
 
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['userID']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE userID = ?', (user_id,)
+        ).fetchone()
