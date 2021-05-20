37c474dd697a766540d3f9c181616c668d7aa8e1
Nick White <nick@nickpale.com>
2021-05-19 23:22:00 -0700

    Reorganize repository and add react site

--- cdk/lib/phial-stack.js
+++ cdk/lib/phial-stack.js
@@ -2,16 +2,21 @@ const core = require("@aws-cdk/core");
 const apigateway = require("@aws-cdk/aws-apigateway");
 const lambda = require("@aws-cdk/aws-lambda");
 // const s3 = require("@aws-cdk/aws-s3");
+const spa = require("cdk-spa-deploy");
 
 class PhialStack extends core.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    // const site = new s3.Bucket(this, "PhialFrontEnd");
+    new spa.SPADeploy(this, "PhialSite").createSiteFromHostedZone({
+      zoneName: "timephial.com",
+      indexDoc: "index.html",
+      websiteFolder: "../site/build",
+    });
 
     const flaskApp = new lambda.Function(this, "PhialFlask", {
       runtime: lambda.Runtime.PYTHON_3_8,
-      code: lambda.Code.fromAsset("api", {
+      code: lambda.Code.fromAsset("../api", {
         bundling: {
           image: lambda.Runtime.PYTHON_3_8.bundlingImage,
           command: [
