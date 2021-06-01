35495cfc0c3df6f27ddbaafb39f9d0751e196e4b
Nick White <nick@nickpale.com>
2021-05-31 22:59:25 -0700

    Create pipeline cdk stack for CI/CD

---
+++ cdk/lib/pipeline-stack.js
@@ -0,0 +1,82 @@
+import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import { SecretValue, Stack } from '@aws-cdk/core';
+
+export class PipelineStack extends Stack {
+  constructor(app, id, props) {
+    super(app, id, props);
+
+    const cdkBuild = new codebuild.PipelineProject(this, 'CdkBuild', {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            commands: 'npm install',
+          },
+          build: {
+            commands: [
+              'npm run build',
+              'npm run cdk synth -- -o dist'
+            ],
+          },
+        },
+        artifacts: {
+          'base-directory': 'dist',
+          files: [
+            'PhialStack.template.json',
+          ],
+        },
+      }),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+      },
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+    const cdkBuildOutput = new codepipeline.Artifact('CdkBuildOutput');
+    new codepipeline.Pipeline(this, 'Pipeline', {
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [
+            new codepipeline_actions.GitHubSourceAction({
+                actionName: 'GitHub',
+                branch: props.githubBranch,
+                output: sourceOutput,
+                oauthToken: SecretValue.secretsManager('github-token'),
+                owner: props.githubOwner,
+                repo: props.githubRepo,
+            }),
+          ],
+        },
+        {
+          stageName: 'Build',
+          actions: [
+            new codepipeline_actions.CodeBuildAction({
+              actionName: 'CDK_Build',
+              project: cdkBuild,
+              input: sourceOutput,
+              outputs: [cdkBuildOutput],
+            }),
+          ],
+        },
+        {
+          stageName: 'Deploy',
+          actions: [
+            new codepipeline_actions.CloudFormationCreateUpdateStackAction({
+              actionName: 'Lambda_CFN_Deploy',
+              templatePath: cdkBuildOutput.atPath('PhialStack.template.json'),
+              stackName: 'PhialDeploymentStack',
+              adminPermissions: true,
+              parameterOverrides: {
+                ...props.lambdaCode.assign(lambdaBuildOutput.s3Location),
+              },
+              extraInputs: [lambdaBuildOutput],
+            }),
+          ],
+        },
+      ],
+    });
+  }
+}
